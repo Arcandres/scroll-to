93572e1fa92817f64c18a657cc50033c7c50400a
Andres Ardila <Arcandres@hotmail.com>
2018-06-09 15:47:34 -0500

    Adding acceleration to scroll

--- es6/scroll.js
+++ es6/scroll.js
@@ -2,12 +2,13 @@ const scroll = (e,height = 20) => {
   const el = document.getElementById(e.target.hash.slice(1));
 
   // Check if the element exists
-  if (el) {
-    let tf         = true;
-    const initial  = () => document.body.scrollTop,
-          final    = () => Math.floor(el.getBoundingClientRect().y + initial() - height),
-          y        = 10,
-          animate  = setInterval(() => {
+  if (el) { 
+    let tf             = true;
+    const initial      = () => document.body.scrollTop,
+          final        = () => Math.floor(el.getBoundingClientRect().y + initial() - height),
+          acceleration = Math.ceil(final() - initial() / 400),
+          y            = 10,
+          animate      = setInterval(() => {
             if (tf) {
               if (initial() < final()) {
                 window.scrollBy(0, y);
